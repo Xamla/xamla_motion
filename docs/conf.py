d2074a15116a4f6393f229282877dbb3a3bc833f
Manuel Volk <manuel.volk@xamla.com>
2019-05-14 13:40:25 +0200

    add logo to docs

--- docs/conf.py
+++ docs/conf.py
@@ -95,7 +95,11 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+  'logo_only': True,
+  'style_nav_header_background': '#57678d'
+}
+html_logo = "_static/rosvita-docs-logo.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
